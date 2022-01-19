9f6ace2390a9196ad5fdec0b9103cd2b658f9c79
Heran Aregawi <heranaregawi@gmail.com>
2022-01-19 12:36:27 -0600

    Address class is updated

--- src/business/Address.java
+++ src/business/Address.java
@@ -1,5 +1,49 @@
 package business;
 
 public class Address {
+	private String street;
+	private String city;
+	private String state;
+	private int zip;
+
+	public Address(String Street, String City, String State, int zip) {
+		this.street = Street;
+		this.city = City;
+		this.state = State;
+		this.zip = zip;
+
+	}
+
+	public String getStreet() {
+		return this.street;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
+	public int getZip() {
+		return this.zip;
+	}
+
+	public void setStreet(String Street) {
+		this.street = Street;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
 
 }
